d0099e24b95bfd50834bddec149786a4e08472b3
Nadezhda Bereza <a-river@bk.ru>
2023-01-31 13:55:33 +0300

    lesson 1 task  4

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Ведите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите число C: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int  maxNumber = numberA;
+if (numberB > maxNumber) {
+    maxNumber = numberB;
+}
+if (numberC > maxNumber) {
+    maxNumber = numberC;
+}
+Console.WriteLine($"{numberA},{numberB},{numberC} -> {maxNumber}");
