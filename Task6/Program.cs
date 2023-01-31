65bd38d5dfa8eb53c6eea7a7bafddd116afab163
Nadezhda Bereza <a-river@bk.ru>
2023-01-31 14:38:35 +0300

    lesson  1 task 6

---
+++ Task6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Ведите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+string evenName = numberA % 2 == 0 ? "да" : "нет";
+Console.WriteLine($"{numberA} -> {evenName}");
