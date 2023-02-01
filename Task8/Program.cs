b95d013f1a404c91fc7506e10bb55b864c3e1c73
Nadezhda Bereza <a-river@bk.ru>
2023-02-01 13:25:19 +0300

    lesson1 Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая на вход принимает число (N), а на выходе 
+показывает все чётные числа от 1 до N. */
+Console.Clear();
+Console.Write("Ведите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{number} -> ");
+int i = 1;
+bool isFirst = true;
+while (i <= number) {
+    if (i % 2 == 0) {
+        string prefix = isFirst ? "" : ", ";
+        Console.Write($"{prefix}{i}");
+        if (isFirst) {
+            isFirst = false;
+        }
+    }
+    i++;
+}
